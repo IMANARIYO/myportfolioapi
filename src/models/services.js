e24b1f26c8c4d794f83457bb1a1eebb5e349e89f
IMANARIYO <imanariyobaptiste@gmail.com>
2024-09-30 12:09:55 +0200

    adding the  sercise  revised structure

--- src/models/services.js
+++ src/models/services.js
@@ -1,30 +1,58 @@
-import mongoose from 'mongoose';
 import dotenv from "dotenv";
-dotenv.config();
+import mongoose from "mongoose";
 import { v2 as cloudinary } from "cloudinary";
+
+dotenv.config();
+
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET
 });
-const {Schema,model} = mongoose;
+
+const { Schema, model } = mongoose;
+
+// Define the service schema
 const serviceSchema = new Schema({
   name: {
     type: String,
-    required: false 
+   
   },
   image: {
     type: String,
-    required: false 
+   
+  },
+  alt: {
+    type: String,
+   
+  },
+  title: {
+    type: String,
+   
   },
   description: {
     type: String,
-    required: false 
+   
+  },
+  overlayTitle: {
+    type: String,
+    
+  },
+  overlayDescription: {
+    type: String,
+    
+  },
+  overlayLink: {
+    type: String,
+   
   },
   beneficiaries: {
     type: [String],
-    default: []
+    
   }
 }, { timestamps: true }).set('strictPopulate', false);
+
+// Create the model from the schema
 const Service = model('Service', serviceSchema);
+
 export default Service;
